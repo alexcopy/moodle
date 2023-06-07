8717617215d886fd8b70be6fb3cd0ef1bc16ffe9
Alex Redko <a.redko@nr-online.com>
2023-06-07 12:06:51 +0100

    update

--- src/main/java/com/moodle/BrowserDownload.java
+++ src/main/java/com/moodle/BrowserDownload.java
@@ -1,49 +1,87 @@
 package com.moodle;
 
 import com.moodle.DownloadManagerInt;
+
 public class BrowserDownload implements DownloadManagerInt {
 
     private final long fileSize;
     private long receivedSize;
+    private long paketGroesse;
 
     public BrowserDownload(long fileSize) {
         this.fileSize = fileSize;
         this.receivedSize = 0;
     }
 
+    /**
+     * getFileSize()
+     * @return long
+     */
     @Override
     public long gibGroesse() {
         return fileSize;
     }
 
+    /**
+     * getDownloadProgress()
+     * @return double
+     */
     @Override
     public double gibFortschritt() {
-        return (double) receivedSize / fileSize;
+        double result;
+        if (fileSize == 0) {
+            result = 0;
+        } else {
+            result = (double) receivedSize / fileSize;
+        }
+        return result;
     }
 
+    /**
+     * isDownloadComplete()
+     * @return bool
+     */
     @Override
     public boolean istFertig() {
         return receivedSize == fileSize;
     }
 
+    /**
+     * getRemainingSize()
+     * @return long
+     */
     @Override
     public long gibVerbleibend() {
         return fileSize - receivedSize;
     }
 
+
+    /**
+     * markNewPacketAsReceived()
+     * @param paketGroesse die Anzahl der neu empfangenen Bytes
+     */
     @Override
     public void empfange(long paketGroesse) {
-        receivedSize += paketGroesse;
-    }
-
-    public void paketVollstaendigGespeichert(long paketGroesse) {
         if (paketGroesse < 0) {
-            throw new IllegalArgumentException("Invalid packet size: " + paketGroesse);
+            throw new IllegalArgumentException("Packet size cannot be negative");
         }
         if (paketGroesse > gibVerbleibend()) {
             throw new IllegalArgumentException("Packet size exceeds remaining size");
         }
-        empfange(paketGroesse);
+        receivedSize += paketGroesse;
     }
 
+
+    /**
+     * loadNewPackage()
+     * @param paketGroesse
+     */
+    public void ladeNeuesPaket(long paketGroesse) {
+        this.paketGroesse = paketGroesse;
+        if (istFertig()) {
+            throw new IllegalStateException("Download is already complete");
+        }
+        empfange(paketGroesse);
+    }
 }
+
--- src/test/com/moodle/BrowserDownloadTest.java
+++ src/test/com/moodle/BrowserDownloadTest.java
@@ -6,84 +6,81 @@ import org.junit.jupiter.api.Test;
 
 public class BrowserDownloadTest {
 
+    private static final long FILE_SIZE = 1000;
+
     private BrowserDownload download;
 
     @BeforeEach
-    public void setup() {
-        long fileSize = 1000;
-        download = new BrowserDownload(fileSize);
+    public void setUp() {
+        download = new BrowserDownload(FILE_SIZE);
     }
 
     @Test
     public void testGibGroesse() {
-        long expectedSize = 1000;
         long actualSize = download.gibGroesse();
-        Assertions.assertEquals(expectedSize, actualSize);
+        Assertions.assertEquals(FILE_SIZE, actualSize);
     }
 
     @Test
     public void testGibFortschritt() {
-        double expectedProgress = 0.0;
         double actualProgress = download.gibFortschritt();
-        Assertions.assertEquals(expectedProgress, actualProgress);
-
-        download.empfange(500);
-        expectedProgress = 0.5;
-        actualProgress = download.gibFortschritt();
-        Assertions.assertEquals(expectedProgress, actualProgress);
-
-        download.empfange(300);
-        expectedProgress = 0.8;
-        actualProgress = download.gibFortschritt();
-        Assertions.assertEquals(expectedProgress, actualProgress);
+        Assertions.assertEquals(0, actualProgress);
     }
 
     @Test
     public void testIstFertig() {
-        Assertions.assertFalse(download.istFertig());
-
-        download.empfange(1000);
-        Assertions.assertTrue(download.istFertig());
+        boolean isFinished = download.istFertig();
+        Assertions.assertFalse(isFinished);
     }
 
     @Test
     public void testGibVerbleibend() {
-        long expectedRemaining = 1000;
-        long actualRemaining = download.gibVerbleibend();
-        Assertions.assertEquals(expectedRemaining, actualRemaining);
-
-        download.empfange(500);
-        expectedRemaining = 500;
-        actualRemaining = download.gibVerbleibend();
-        Assertions.assertEquals(expectedRemaining, actualRemaining);
+        long remainingSize = download.gibVerbleibend();
+        Assertions.assertEquals(FILE_SIZE, remainingSize);
     }
 
     @Test
-    public void testEmpfange() {
-        download.empfange(500);
-        double expectedReceivedSize = 0.5;
-        double actualReceivedSize = download.gibFortschritt();
-        Assertions.assertEquals(expectedReceivedSize, actualReceivedSize);
-
-        download.empfange(300);
-        expectedReceivedSize = 0.8;
-        actualReceivedSize = download.gibFortschritt();
-        Assertions.assertEquals(expectedReceivedSize, actualReceivedSize);
+    public void testEmpfange_ValidPacketSize() {
+        long packetSize = 500;
+        download.empfange(packetSize);
+        long remainingSize = download.gibVerbleibend();
+        Assertions.assertEquals(FILE_SIZE - packetSize, remainingSize);
     }
 
     @Test
-    public void testPaketVollstaendigGespeichert() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> download.paketVollstaendigGespeichert(-100));
+    public void testEmpfange_NegativePacketSize() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> download.empfange(-100));
+    }
 
-        Assertions.assertThrows(IllegalArgumentException.class, () -> download.paketVollstaendigGespeichert(1100));
-        download.paketVollstaendigGespeichert(500);
-        double expectedReceivedSize = 0.5;
-        double actualReceivedSize = download.gibFortschritt();
-        Assertions.assertEquals(expectedReceivedSize, actualReceivedSize);
+    @Test
+    public void testEmpfange_ExceedingPacketSize() {
+        long packetSize = FILE_SIZE + 100;
+        Assertions.assertThrows(IllegalArgumentException.class, () -> download.empfange(packetSize));
+    }
 
-        download.paketVollstaendigGespeichert(300);
-        expectedReceivedSize = 0.8;
-        actualReceivedSize = download.gibFortschritt();
-        Assertions.assertEquals(expectedReceivedSize, actualReceivedSize);
+    @Test
+    public void testLadeNeuesPaket_ValidPacketSize() {
+        long packetSize = 500;
+        download.ladeNeuesPaket(packetSize);
+        long remainingSize = download.gibVerbleibend();
+        Assertions.assertEquals(FILE_SIZE - packetSize, remainingSize);
+    }
+
+    @Test
+    public void testLadeNeuesPaket_NegativePacketSize() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> download.ladeNeuesPaket(-100));
+    }
+
+    @Test
+    public void testLadeNeuesPaket_ExceedingPacketSize() {
+        long packetSize = FILE_SIZE + 100;
+        Assertions.assertThrows(IllegalArgumentException.class, () -> download.ladeNeuesPaket(packetSize));
+    }
+
+    @Test
+    public void testLadeNeuesPaket_WhenDownloadComplete() {
+        long packetSize = FILE_SIZE;
+        download.ladeNeuesPaket(packetSize);
+        Assertions.assertThrows(IllegalStateException.class, () -> download.ladeNeuesPaket(packetSize));
     }
 }
